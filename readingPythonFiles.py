88cfba152b46d4bd436226e2dd4ed8f96bdc36c3
Gustavo Cirne Ferreira <114815870+GustavoCirneFerreira@users.noreply.github.com>
2024-07-22 14:17:14 -0300

    Atividade sobre ler e criar arquivos python

--- readingPythonFiles.py
+++ readingPythonFiles.py
@@ -1,9 +1,27 @@
-caminho_arquivo = 'C:\\Users\\Túlio Cirne\\Desktop\\pasta_teste\\'
+caminho_arquivo = r'C:\\Users\\Túlio Cirne\\Desktop\\pasta_teste\\'
 caminho_arquivo += 'arquivo.txt'
 
 # arquivo = open(caminho_arquivo, 'w')
 # arquivo.close()
 
-with open(caminho_arquivo, 'w') as arquivo:
-    print('Olá Mundo!')
-    print('Arquivo fechado!')
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+    arquivo.seek(0,0)
+    print(arquivo.read())
+    print('Lendo...')
+    arquivo.seek(0,0)
+    print(arquivo.readline(), end='')
+    print(arquivo.readline().strip())
+    print(arquivo.readline().strip())
+
+    print('READLINES')
+    arquivo.seek(0,0)
+    for linha in arquivo.readlines():
+        print(linha.strip())
+
+with open(caminho_arquivo, 'r') as arquivo:
+    print(arquivo.read())
