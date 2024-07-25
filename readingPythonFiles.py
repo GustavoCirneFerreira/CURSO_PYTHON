65483b776dfd2c9758425a5163c6ceb42a5c0fb2
Gustavo Cirne Ferreira <114815870+GustavoCirneFerreira@users.noreply.github.com>
2024-07-25 16:11:50 -0300

    Implementações a leitura e escrita de arquivos python

--- readingPythonFiles.py
+++ readingPythonFiles.py
@@ -1,27 +1,43 @@
-caminho_arquivo = r'C:\\Users\\Túlio Cirne\\Desktop\\pasta_teste\\'
+import os
+
+caminho_arquivo = r'C:\\Users\\Túlio Cirne\\Desktop\\CURSO_PYTHON\\'
 caminho_arquivo += 'arquivo.txt'
 
 # arquivo = open(caminho_arquivo, 'w')
 # arquivo.close()
 
-with open(caminho_arquivo, 'w+') as arquivo:
-    arquivo.write('Linha 1\n')
-    arquivo.write('Linha 2\n')
-    arquivo.writelines(
-        ('Linha 3\n', 'Linha 4\n')
-    )
-    arquivo.seek(0,0)
-    print(arquivo.read())
-    print('Lendo...')
-    arquivo.seek(0,0)
-    print(arquivo.readline(), end='')
-    print(arquivo.readline().strip())
-    print(arquivo.readline().strip())
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     print(type(arquivo))
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'Linha 4\n')
+#     )
+#     arquivo.seek(0,0)
+#     print(arquivo.read())
+#     print('Lendo...')
+#     arquivo.seek(0,0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
 
-    print('READLINES')
-    arquivo.seek(0,0)
-    for linha in arquivo.readlines():
-        print(linha.strip())
+#     print('READLINES')
+#     arquivo.seek(0,0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
 
-with open(caminho_arquivo, 'r') as arquivo:
-    print(arquivo.read())
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+
+
+with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+     ...
+     print(type(arquivo))
+     arquivo.write('Atenção\n')
+     arquivo.write('Linha 2\n')
+     arquivo.writelines(
+         ('Linha 3\n', 'Linha 4\n')
+     )
+
+# os.remove(caminho_arquivo)
+os.rename(caminho_arquivo, 'aula115.txt')
