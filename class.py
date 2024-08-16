5887eab21ffe237a89ddf39521817d4c3f475858
Gustavo Cirne Ferreira <gustavocirne9@gmail.com>
2024-08-16 16:00:39 -0300

    Aprendizado sobre classes

---
+++ class.py
@@ -0,0 +1,18 @@
+string = 'Gustavo'
+
+class Pessoa:
+    ...
+
+p1 = Pessoa()
+p1.nome = 'Gustavo'
+p1.sobrenome = 'Cirne'
+
+p2 = Pessoa()
+p2.nome = 'Roberto'
+p2.sobrenome = 'Vargas'
+
+print(p1.nome)
+print(p1.sobrenome)
+
+print(p2.nome)
+print(p2.sobrenome)
