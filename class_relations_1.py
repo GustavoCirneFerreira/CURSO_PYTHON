5f265aec62fbbfb6074bd66d7cd8af256c4fd0f0
Gustavo Cirne Ferreira <gustavocirne9@gmail.com>
2024-09-12 15:38:40 -0300

    Exercícios sobre associação de classes

---
+++ class_relations_1.py
@@ -0,0 +1,35 @@
+# Aggregation:
+
+class Escritor():
+    def __init__(self, nome):
+        self.nome = nome
+        self._ferramenta = None
+
+    @property
+    def ferramenta(self):
+        return self._ferramenta
+    
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self._ferramenta = ferramenta
+
+
+class Ferramenta():
+    def __init__(self, nome):
+        self.nome = nome
+
+    def escrever(self):
+        return f'{self.nome} está escrevendo...'
+    
+escritor = Escritor('Renato')
+
+lapis = Ferramenta('Lápis de desenho')
+
+maquina_de_escrever = Ferramenta('Máquina de escrever')
+
+escritor.ferramenta = maquina_de_escrever
+
+print(lapis.escrever())
+print(maquina_de_escrever.escrever())
+
+print(escritor.ferramenta.escrever())
