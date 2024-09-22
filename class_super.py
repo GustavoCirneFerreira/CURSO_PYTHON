5e0e2d4f63ce96f7daa499d66e179045ee04a73e
Gustavo Cirne Ferreira <gustavocirne9@gmail.com>
2024-09-22 16:18:16 -0300

    Aulas sobre herança múltipla, abstrição e mixins

--- class_super.py
+++ class_super.py
@@ -14,23 +14,43 @@
 # string = MinhaString('Luiz')
 # print(string.upper())
 
-class A:
+class A(object):
     atributo_a = 'valor a'
+
+    def __init__(self, atributo):
+        self.atributo = atributo
+
+
     def metodo(self):
         print('A')
 
 class B(A):
     atributo_b = 'valor b'
+
+    def __init__(self, atributo, outra_coisa):
+        super().__init__(atributo)
+        self.outra_coisa = outra_coisa
+
     def metodo(self):
         print('B')
 
 class C(B):
     atributo_c = 'valor c'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print('EI, burlei o sistema')
+
     def metodo(self):
-        super(C, self).metodo()
+        super(B, self).metodo()
         print('C')
 
-c = C()
-print(c.atributo_a)
-print(c.atributo_b)
-print(c.atributo_c)
+# print(C.mro())
+# print(B.mro())
+c = C('ATRIBUTO', 'Qualquer')
+print(c.atributo)
+print(c.outra_coisa)
+# print(c.atributo_a)
+# print(c.atributo_b)
+# print(c.atributo_c)
+# c.metodo()
